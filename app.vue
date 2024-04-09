adcca4781500544fd044f52679898ddf20aad147
Iván Manzaba <mauricio.9.inm@gmail.com>
2024-04-08 22:47:16 -0500

    Actualizar estilos de navegación y agregar soporte para PWA

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
-  <VitePwaManifest />
+  <!-- <VitePwaManifest /> -->
+  <NuxtPwaManifest />
   <div class="fixed inset-0 flex overflow-hidden">
     <Navigator class="flex-col items-stretch relative w-full border-b lg:border-b-0 lg:border-r border-gray-200 dark:border-gray-800 lg:w-[--width] flex-shrink-0 hidden lg:flex" />
     <UContainer class="flex flex-1 w-full min-w-0 overflow-y-auto" >
@@ -7,6 +8,15 @@
         <NuxtPage />
       </div>
     </UContainer>
+  </div>
+  <div v-show="$pwa?.needRefresh">
+    <span>
+      Nuevo contenido disponible, recargue la página.
+    </span>
+
+    <button @click="$pwa?.updateServiceWorker()">
+      Recargar
+    </button>
   </div>
 </template>
 <script setup lang="ts">
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import process from 'node:process'
+
+const sw = process.env.SW === 'true'
 export default defineNuxtConfig({
   devtools: {
     enabled: true,
@@ -14,10 +17,23 @@ export default defineNuxtConfig({
     }
   },
   pwa: {
+    strategies: sw ? 'injectManifest' : 'generateSW',
+    srcDir: sw ? 'service-worker' : undefined,
+    filename: sw ? 'sw.ts' : undefined,
+    registerType: 'autoUpdate',
     manifest: {
-      name: 'My Nuxt App',
-      short_name: 'My Nuxt App',
-      lang: 'es'
+      name: 'Damai Apartments',
+      short_name: 'Damai',
+      lang: 'es',
+      display: 'standalone',
+      theme_color: '#678265',
+      icons: [
+        {
+          src: 'logo_damai.jpg',
+          sizes: '1024x1024',
+          type: 'image/jpg',
+        },
+      ],
     }
   }
 })
